e966b72845de48f6dcfbe6cf034d50de62c79488
Douth <47131318+douth-c@users.noreply.github.com>
2022-05-02 18:51:35 -0500

    refractor: added monke construct page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import monkeGif from './monke.gif';
+
 function App() {
   return (
-    <div className="main">
+    <div className="App">
+      <div className="Interior">
+        <div className="Construct">
+          <img className="monkeGif" src={monkeGif} alt="monkeGif"/>
+          <div className="Construct-Text">
+            This webpage is not yet available. <br/>
+            Enjoy this gif of a chimp licking your monitor.
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
